aed6f3ab836f5b4af8a1bc0e3d9550cbfde9bfb7
martin <mandujar@fi.uba.ar>
2017-11-11 06:50:04 -0300

    Se logra agregar el fancybox correctamente

--- BookingRecommendationSystem/pagina/public/app/scripts/controllers/init/welcomeCtrl.js
+++ BookingRecommendationSystem/pagina/public/app/scripts/controllers/init/welcomeCtrl.js
@@ -1,65 +1,59 @@
 
 
+	function init() {
+		$('#app').empty().load('app/templates/home.html');
+		$('body').attr('style',`
+			background-image : url("/app/images/travel.jpg");
+			background-size: 100% 120%;
+		`);	
 
-function init() {
-	$('#app').empty().load('app/templates/home.html');
-	$('body').attr('style',`
-		background-image : url("/app/images/travel.jpg");
-		background-size: 100% 120%;
-	`);	
-
-	addHandlers();
-}
+		addHandlers();
+	}
 
 
-function addHandlers() {
-	$(document).on('click', '#infoImage', function(e) {
-		openFancyBox();
-	})
-}
+	function addHandlers() {
+		$(document).on('click', '#infoImage', function(e) {
+			openFancyBox();
+		})
+	}
 
-function openFancyBox() {
-	let content = `
-		<div class="tophometour">
-            <div class="titleimghometour">
-	            <p class="titlehometour">YUCATAN - MEXICO</p>
-	            <img alt="" class="imghometour" src="img/tours/tour2.jpg">
-	        </div>
-	        <div class="datedescriptionhometour">
-	            <div class="datedayhometour">
-	                <p class="datehometour">
-	                    JANUARY - 3 to 17
-	                </p>
-	                <div class="dayhometour">
-	                    <p>15</p>
-	                    <span>DAYS</span>
-	                </div>
-	            </div>
-	            <div class="descriptionhometour" style="overflow: hidden; height: 141px;" tabindex="5001">
-	                <p><strong class="titledescriptionhometour">Title description</strong><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus ultrices purus, volutpat malesuada sapien sollicitudin vitae.</p>
-	            </div>	
-	        </div>	
-        </div>
-	`
-	$(document).on('click', '#trip-open').fancybox({
-	  content : 'sdsds',
-	  closeBtn: true,
-			closeClick: false,
-			height: 400,
-			width: 400,
-			autoSize: false,
-	  helpers: {
-	      overlay: {
-	          closeClick: false
-	      }
-	  },
-	  keys: {
-	      close: null
-	  }
-	}).trigger('click');
-	$.fancybox.open();
+	function openFancyBox() {
+		let content = `
+		<div class="landscape">
+			<div class="row">
+				  <div class="col-xs-6 col-md-6">
+						<img src="/app/images/desert.jpg" style="width: 107%;margin-right: 45%;">
+				</div>	
+						<div class="col-xs-6 col-md-6" >
+							(Aca iria la descripción del lugar)
+						</div>
+						
+			        </div>
+			<button class="btn btn-primary bottom-space" style="">Reservar Lugar</button></div>
+		</div>
+		`;
 
-	$('#trip-out').click(function () {
-	    $.fancybox.close();
-	});
+		// Set custom style, close if clicked, change title type and overlay color
+		$("#trip-open").fancybox({
+		 	content :  content,
+			wrapCSS    : 'fancybox-custom',
+			closeClick : true,
+			closeBtn : true,
+			arrows : true,
+
+			height : 130,
+			width : 100,
+			autoCenter : true,
+			helpers : {
+				title : {
+					type : 'inside'
+				},
+				overlay : {
+					css : {
+						'background' : 'rgba(238,238,238,0.85)'
+					}
+				}
+			}
+		}).trigger('click').trigger('click');
+
 }
