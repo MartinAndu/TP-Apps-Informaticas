4bd513667bd1389eb8fdd74d3489129ecb38035e
martin <mandujar@fi.uba.ar>
2017-11-22 02:28:45 -0300

    Se agregan unas clases faltantes

---
+++ Java Reco/src/main/java/com/company/LikeParams.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class LikeParams {
+    String userName;
+    String place;
+    String price;
+    String rating;
+    String environment;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+}
